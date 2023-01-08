66d20bfdaaba8bfe46d048574917761adcd10cec
Aryan Gandevia <aryangandevia@aryans-mbp.home>
2023-01-08 03:31:02 -0500

    hello

--- pages/_document.js
+++ pages/_document.js
@@ -3,11 +3,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html>
-      
+
       {/* Imported font for everything*/}
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
         <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet"/>
       </Head>
       <body>
